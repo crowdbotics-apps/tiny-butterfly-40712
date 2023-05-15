6d4d2fe8cbcf91c8277904d7df1b15470b8e595a
Crowdbotics <team@crowdbotics.com>
2023-05-15 19:18:09 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/tiny_butterfly_40712/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tiny_butterfly_40712;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "tiny_butterfly_40712";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tiny-butterfly-40712",
+  url: "https://tiny-butterfly-40712.botics.co",
+  api: "https://tiny-butterfly-40712.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
